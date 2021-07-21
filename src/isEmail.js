a83118d7d85431d28ed71613877de4f2172e5a99
ready-research <72916209+ready-research@users.noreply.github.com>
2021-07-21 13:10:26 +0530

    Security fix for ReDoS

--- src/isEmail.js
+++ src/isEmail.js
@@ -2,9 +2,10 @@ import compose from './compose';
 import isString from './isString';
 import matchesPattern from './matchesPattern';
 
+// https://www.regular-expressions.info/email.html
 const isEmail = compose(
   isString,
-  matchesPattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/),
+  matchesPattern(/^[a-z0-9!#$%&'*+/=?^_‘{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_‘{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/),
 );
 
 export default isEmail;
